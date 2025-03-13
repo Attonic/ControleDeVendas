c351f5ea33b30635737f36a19c40bae34bb74322
attonic <a.cleisonn@gmail.com>
2025-03-12 22:35:22 -0300

    Na Classe menu: Organizado as opções do menu, o switch, adicionado um metodo registetClients para opção 1 de cadastro de clientes

--- src/com/controleVendas/main/Menu.java
+++ src/com/controleVendas/main/Menu.java
@@ -1,6 +1,13 @@
 package com.controleVendas.main;
 
 
+import com.controleVendas.entities.Client;
+import com.controleVendas.entities.Product;
+import com.controleVendas.utils.SaleManager;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,40 +15,50 @@ public class Menu {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        SaleManager saleManager = new SaleManager();
+
         int op;
         do {
-            System.out.println("------- Controle de Vendas ------");
+            System.out.println("\n------- Controle de Vendas --------");
             System.out.println();
-            System.out.println("---  1 - Cadastrar Cliente: -----");
-            System.out.println("---  2 - Cadastrar Produto: -----");
-            System.out.println("---  3 - Consultar Cliente: -----");
-            System.out.println("---  4 - Consultar Produto: -----");
-            System.out.println("---  5 - Vender: ");
-            System.out.println("---  6 - Consultar Vendas:  -----");
-            System.out.println("---  7 - Alterar Estoque:   -----");
+            System.out.println("---  1 - Cadastrar Cliente: ---------");
+            System.out.println("---  2 - Cadastrar Produto: ---------");
+            System.out.println("---  3 - Consultar Cliente: ---------");
+            System.out.println("---  4 - Consultar Produto: ---------");
+            System.out.println("---  5 - Vender: --------------------");
+            System.out.println("---  6 - Consultar Vendas: ----------");
+            System.out.println("---  7 - Alterar Estoque:  ----------");
             System.out.println("---  8 - Consultar por nome Cliente: ");
-            System.out.println("---  9 - Sair:  -----");
+            System.out.println("---  9 - Sair:  ---------------------");
             System.out.print("---- Selecione uma opção: ");
             op = input.nextInt();
+            input.nextLine();
 
             switch(op){
-                case 1:
+
+                case 1:  //Cadastrar Clientes
+                    registerCliente(input, saleManager);
                     break;
-                case 2:
+                case 2: // Cadastrar Produto
                     break;
-                case 3:
+                case 3: // Consultar Cliente
+                    saleManager.displayClients();
                     break;
-                case 4:
+                case 4: // Consultar Produto
+                    saleManager.displayProducts();
                     break;
-                case 5:
+                case 5: //  Vender
+
                     break;
-                case 6:
+                case 6: //  Consultar Vendas
+                    saleManager.displayOrders();
                     break;
-                case 7:
+                case 7: //   Alterar Estoque
                     break;
-                case 8:
+
+                case 8:  //   Consultar por nome Cliente
                     break;
-                case 9:
+                case 9: //    Sair
                     op = 9;
                     break;
             }
@@ -52,5 +69,28 @@ public class Menu {
 
 
 
+    }
+
+    private static void registerCliente(Scanner input, SaleManager saleManager){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.print("Digite o nome Cliente: ");
+        String name = input.nextLine();
+
+        LocalDate birthDate = null;
+
+        while (true){
+            System.out.print("Digite a data de Nascimento do cliente: ");
+            try {
+                birthDate = LocalDate.parse(input.nextLine(), formatter);
+                break;
+            }catch (DateTimeException e){
+                System.out.println("A data é invalida, use o formato: dd/MM/yyyy ");
+            }
+        }
+
+        Client client = new Client(name, birthDate);
+        saleManager.registerClient(client);
+        System.out.println("Cliente Cadastrado!");
+        System.out.println(client);
     }
 }
